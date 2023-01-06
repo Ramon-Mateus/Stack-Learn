bf8b6e1aeec7ad5067f89b74d97834740700807c
Vinícius César <v.cesar@academico.ifrn.edu.br>
2023-01-05 23:59:05 -0300

    Autenticação - Parte1

---
+++ Stack_Learn/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Stack_Learn.Areas.Seguranca.Models;
+using System.Data.Entity;
+
+namespace Stack_Learn.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb") { }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+        //public System.Data.Entity.DbSet<Stack_Learn.Areas.Seguranca.Models.Usuario> Usuarios { get; set; }
+    }
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao> { }
+}
---
+++ Stack_Learn/Infraestrutura/GerenciadorUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Stack_Learn.Areas.Seguranca.Models;
+using Stack_Learn.DAL;
+
+namespace Stack_Learn.Infraestrutura
+{
+    public class GerenciadorUsuario : UserManager<Usuario>
+    {
+        public GerenciadorUsuario(IUserStore<Usuario> store) : base(store) { }
+        public static GerenciadorUsuario Create(IdentityFactoryOptions<GerenciadorUsuario> options, IOwinContext context)
+        {
+            IdentityDbContextAplicacao db = context.Get<IdentityDbContextAplicacao>();
+            GerenciadorUsuario manager = new GerenciadorUsuario(new UserStore<Usuario>(db));
+            return manager;
+        }
+    }
+}
